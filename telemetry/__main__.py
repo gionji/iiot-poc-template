464e4b5753503e8ff36268a76a3b9dae640b7356
gionji <giovanniburresi@gmail.com>
2020-07-20 17:55:09 +0200

    tentata la telemetry

---
+++ telemetry/__main__.py
@@ -0,0 +1,64 @@
+import json
+from datetime import datetime
+from time import sleep
+
+from libs.utils import connector, IIoT
+
+
+MY_DEVICE_ID         = ''
+MY_APP_ACCESS_KEY    = ''
+MY_APP_ACCESS_SECRET = ''
+
+class GenericValue:
+    def __init__(self, key, value, timestamp):
+        self.key       = key
+        self.value     = value
+        self.timestamp = timestamp
+
+    def format(self):
+        return json.dumps({
+            "key"      : self.key,
+            "value"    : self.value,
+            "timestamp": self.timestamp
+        })
+
+
+def command_from_losant_callback(self, command):
+    print(command)
+
+
+if __name__ == "__main__":
+    # Subscribed channels
+    topics = [IIoT.MqttChannels.sensors]
+
+    # Connect to MQTT clients
+    mqtt_client = connector.MqttLocalClient('TELEMETRY-LOSANT', 'localhost', 1883, topics)
+    mqtt_client.start()
+
+
+    ## Init the module core object
+    losant_client = LosantAgent(
+                                  my_device_id         = MY_DEVICE_ID,
+                                  my_app_access_key    = MY_APP_ACCESS_KEY,
+                                  my_app_access_secret = MY_APP_ACCESS_SECRET,
+                                )
+    losant_client.set_callback( command_from_losant_callback )
+    losant_client.name = 'Losant Thread'
+    losant_client.start()
+
+
+    while True:
+        ## Waiting for messages from the subscribed mqtt channels
+        message = mqtt_client.message_queue.get()
+        print(message.payload)
+
+        json_payload = json.loads(message.payload)
+        timestamp    = json_payload['timestamp']
+        key          = json_payload['key']
+        value        = json_payload['value']
+
+
+        ## Publish the data
+        # mqtt_client.publish(IIoT.MqttChannels.sensors, format(reader.key, value, int(datetime.now().timestamp())))
+
+        sleep(10)
---
+++ telemetry/agent.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+
+
+
+# this is an abstract clas. Each db implementation has to implement these three methods
+class TelemetryAgent(ABC):
+
+    @abstractmethod
+    def on_command(self, device, command) -> bool:
+        pass
+
+    @abstractmethod
+    def send_state(self, name, value) -> bool:
+        pass
+
+    @abstractmethod
+    def set_callback(self, callback) -> bool:
+        pass
+
+
+
+class LosantAgent(TelemetryAgent, ABC, threading.Thread):
+
+    def __init__( self, my_device_id, my_app_access_key, my_app_access_secret ):
+        super().__init__()
+        threading.Thread.__init__(self)
+        self.my_device_id         = my_device_id
+        self.my_app_access_key    = my_app_access_key
+        self.my_app_access_secret = my_app_access_secret
+
+        # Construct Losant device
+        self.device = Device( self.my_device_id,
+                        self.my_app_access_key,
+                        self.my_app_access_secret)
+
+        self.callback = None
+
+
+    def run( self ):
+        # Connect to Losant and leave the connection open
+        self.device.add_event_observer("command", self.on_command)
+        self.device.connect(blocking=True)
+
+
+    def send_state(self, name, value):
+        print("Sending Device State")
+        self.device.send_state( { str(name) : value } )
+
+
+    def on_command(self, device, command):
+        print(command["name"] + " command received.")
+
+        if command["name"] == "toggle":
+            self.callback(command)
+            print("Do something")
+
+    def set_callback(self, callback):
+        self.callback = callback
